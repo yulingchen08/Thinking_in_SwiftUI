30f7aa0ce8200794b3c81757550199e739b0d17b
yulingchen08 <yulingchen08@gmail.com>
2021-11-10 08:14:36 +0800

    Initial Commit

---
+++ Shared/Thinking_in_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Thinking_in_SwiftUIApp.swift
+//  Shared
+//
+//  Created by 陳鈺翎 on 2021/11/10.
+//
+
+import SwiftUI
+
+@main
+struct Thinking_in_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
