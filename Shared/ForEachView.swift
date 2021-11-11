c2e29f58125890e4e4e249aaf89cc925bbbb4db5
Eric Chen 陳鈺翎 <eric.yl.chen@gogoro.com>
2021-11-11 17:57:28 +0800

    ForEach view

---
+++ Shared/ForEachView.swift
@@ -0,0 +1,36 @@
+//
+//  ForEachView.swift
+//  Thinking_in_SwiftUI
+//
+//  Created by Eric Chen 陳鈺翎 on 2021/11/11.
+//
+
+import SwiftUI
+
+struct ForEachView: View {
+    var body: some View {
+        
+        ForEach(1...3, id: \.self) { x in
+            Text("Item \(x)")
+        }
+    }
+}
+
+
+
+struct ForEachView_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEachView()
+    }
+}
+
+
+class Master {
+    var pet: Pet
+    init(pet: Pet) {
+        self.pet = pet
+    }
+    func pokePet() {
+        pet.wasPoked()
+    }
+}
